ca81ef0a6e82e1d701b7251ee64211fdfad9d227
woohm402 <woohm404@gmail.com>
2023-03-27 23:28:44 +0900

    add slack app endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,58 @@
-import express, { NextFunction, Request, Response } from "express";
+import crypto from "crypto";
+import dotenv from "dotenv";
+import express from "express";
 
+dotenv.config({ path: ".env.local" });
+
+const signingSecret = process.env.SLACK_SIGNING_SECRET;
 const PORT = 3000;
-
 const app = express();
 
-app.get("/ping", (req: Request, res: Response, next: NextFunction) => {
-  res.send("pong");
+app.post("/slack/action-endpoint", express.json(), (req, res) => {
+  const signature = req.headers["x-slack-signature"];
+  const timestamp = req.headers["x-slack-request-timestamp"];
+  const rawBody = "rawBody" in req ? req.rawBody : undefined;
+
+  try {
+    if (!signingSecret) throw new Error("500");
+    if (!rawBody) throw new Error("400");
+    if (typeof signature !== "string") throw new Error("403");
+    if (Math.abs(Date.now() / 1000 - Number(timestamp)) > 60 * 5)
+      throw new Error("403");
+
+    const hmac = crypto.createHmac("sha256", signingSecret);
+    hmac.update(`v0:${timestamp}:${rawBody}`);
+
+    if (
+      !crypto.timingSafeEqual(
+        Buffer.from(`v0=${hmac.digest("hex")}`),
+        Buffer.from(signature)
+      )
+    ) {
+      res.sendStatus(400);
+      throw new Error("Invalid signature");
+    }
+
+    // Slack event subscription verification
+    if (req.body.type === "url_verification")
+      return res.send(req.body.challenge);
+
+    // Handle other events
+    console.log(req.body.event);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+
+    const message =
+      err && typeof err === "object" && "message" in err
+        ? Number(err.message)
+        : null;
+
+    if (message === null || isNaN(message)) return res.sendStatus(500);
+
+    return res.sendStatus(message);
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+// Start the server
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
