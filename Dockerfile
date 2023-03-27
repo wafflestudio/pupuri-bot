822f2a693b05eb8095e28aa13fbee3020f4786cd
woohm402 <woohm404@gmail.com>
2023-03-27 23:28:44 +0900

    add slack app endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:18.11.0-alpine
+ARG SLACK_BOT_TOKEN
 COPY . .
+RUN echo "SLACK_BOT_TOKEN=${SLACK_BOT_TOKEN}" >> .env.local
 RUN yarn install
 RUN yarn build
 CMD ["yarn", "start"]
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import express from "express";
 
+dotenv.config({ path: ".env.local" });
+
+const token = process.env.SLACK_BOT_TOKEN;
 const PORT = 3000;
-
 const app = express();
 
-app.get("/ping", (req: Request, res: Response, next: NextFunction) => {
-  res.send("pong");
+app.post("/slack/action-endpoint", express.json(), (req, res) => {
+  if (req.body.token !== token) return res.sendStatus(403);
+
+  // Slack event subscription verification
+  if (req.body.type === "url_verification") return res.send(req.body.challenge);
+
+  // Handle other events
+  console.log(req.body.event);
+  res.sendStatus(200);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+// Start the server
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
