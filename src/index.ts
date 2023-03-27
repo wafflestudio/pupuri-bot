3fb30c259e2c90f29f5e01e4fa05225a667d2a69
woohm402 <woohm404@gmail.com>
2023-03-27 23:28:44 +0900

    add slack app endpoint
    add debug

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,37 @@
-import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import express from "express";
 
+dotenv.config({ path: ".env.local" });
+
+const slackBotToken = process.env.SLACK_BOT_TOKEN;
+const slackWatcherChannerId = process.env.SLACK_WATCHER_CHANNEL_ID;
+const slackAuthToken = process.env.SLACK_AUTH_TOKEN;
 const PORT = 3000;
-
 const app = express();
 
-app.get("/ping", (req: Request, res: Response, next: NextFunction) => {
-  res.send("pong");
+app.post("/slack/action-endpoint", express.json(), (req, res) => {
+  if (req.body.token !== slackBotToken) return res.sendStatus(403);
+
+  // Slack event subscription verification
+  if (req.body.type === "url_verification") return res.send(req.body.challenge);
+
+  // Handle other events
+  if (req.body.event.type === "channel_rename") {
+    fetch("https://slack.com/api/chat.postMessage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${slackAuthToken}`,
+      },
+      body: JSON.stringify({
+        channel: slackWatcherChannerId,
+        text: `<#${req.body.event.channel.id}> has renamed.`,
+      }),
+    });
+  }
+
+  res.sendStatus(200);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+// Start the server
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
