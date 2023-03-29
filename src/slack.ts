39527a48d72945f49aa918f2c9828a4a5422436f
woohm402 <woohm404@gmail.com>
2023-03-29 20:36:00 +0900

    setup slack event types

--- src/slack.ts
+++ src/slack.ts
@@ -1,17 +1,28 @@
-export const handleSlackEvent = async (
-  event: { type: string; channel: { id: string } },
-  sendMessage: (text: string) => void
-) => {
-  const channelId = event.channel.id;
+export const handleSlackEvent = async (event: SlackEvent, sendMessage: (text: string) => void) => {
   switch (event.type) {
-    case "channel_created":
-      sendMessage(`<#${channelId}> 채널이 생성되었어요`);
+    case 'channel_archive':
+      sendMessage(`<#${event.channel}> 채널이 보관되었어요`);
       break;
-    case "channel_archive":
-      sendMessage(`<#${channelId}> 채널이 보관되었어요`);
+    case 'channel_created':
+      sendMessage(`<#${event.channel.id}> 채널이 생성되었어요`);
       break;
-    case "channel_unarchive":
-      sendMessage(`<#${channelId}> 채널이 보관 해제되었어요`);
+    case 'channel_rename':
+      sendMessage(`<#${event.channel.id}> 채널이 생성되었어요`);
+      break;
+    case 'channel_unarchive':
+      sendMessage(`<#${event.channel}> 채널이 보관 취소되었어요`);
       break;
   }
 };
+
+type ChannelId = string;
+type UserId = string;
+
+/**
+ * @see https://api.slack.com/events
+ */
+type SlackEvent =
+  | { type: 'channel_archive'; channel: ChannelId; user: UserId }
+  | { type: 'channel_created'; channel: { id: ChannelId; name: string; created: number; creator: UserId } }
+  | { type: 'channel_rename'; channel: { id: ChannelId; name: string; created: number } }
+  | { type: 'channel_unarchive'; channel: ChannelId; user: UserId };
