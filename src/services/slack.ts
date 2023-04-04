a9fffba54ef9aa7e53be888755e3e724fbfb21d2
woohm402 <woohm404@gmail.com>
2023-04-04 21:52:24 +0900

    move SlackEvent to Entity

--- src/services/slack.ts
+++ src/services/slack.ts
@@ -57,16 +57,3 @@ export const getSlackService = ({
 };
 
 const rankEmojis = [':first_place_medal:', ':second_place_medal:', ':third_place_medal:', ':four:', ':five:'];
-
-type ChannelId = string;
-type UserId = string;
-
-/**
- * @see https://api.slack.com/events
- */
-
-type SlackEvent =
-  | { type: 'channel_archive'; channel: ChannelId; user: UserId }
-  | { type: 'channel_created'; channel: { id: ChannelId; name: string; created: number; creator: UserId } }
-  | { type: 'channel_rename'; channel: { id: ChannelId; name: string; created: number } }
-  | { type: 'channel_unarchive'; channel: ChannelId; user: UserId };
