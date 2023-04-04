af148c526e81b67aa69c0fc1bfd8af259242fec1
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-04-04 21:19:08 +0900

    ✨ repository 점수 계산 방식 변경 (#31)

--- src/entities/github.ts
+++ src/entities/github.ts
@@ -2,5 +2,14 @@ export type Repository = {
   name: string; // waffle-world
   url: string; // https://api.github.com/repos/wafflestudio/waffle-world
   html_url: string; // https://github.com/wafflestudio/waffle-world
+  updated_at: string; // '2011-01-26T19:01:12Z';
 };
-export type Commit = { sha: string };
+export type PullRequest = {
+  title: string;
+  updated_at: string; // '2011-01-26T19:01:12Z';
+  merged_at: string | null; // '2011-01-26T19:01:12Z';
+};
+export type Comment = {
+  body: string; // 'LGTM!'
+  created_at: string; // '2011-01-26T19:01:12Z';
+};
