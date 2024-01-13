1a201cd59871bdb6505a4a4d1e6362e00fce600f
양주현 <didwngus01@naver.com>
2024-01-13 18:25:16 +0900

    작업

--- src/services/DashboardService.ts
+++ src/services/DashboardService.ts
@@ -1,3 +1,4 @@
 export type DashboardService = {
   sendGithubTopRepositoriesLastWeek: (organization: string) => Promise<void>;
+  sendGithubTopRepositoriesPerTeamLastDay: (organization: string) => Promise<void>;
 };
