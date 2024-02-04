9b822449c269b58a75812430e9c163cb96198f9f
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-02-04 23:34:58 +0900

    🔥 토이프로젝트 3일 알리미 제거 (#48)

--- src/services/DashboardService.ts
+++ src/services/DashboardService.ts
@@ -1,4 +1,3 @@
 export type DashboardService = {
   sendGithubTopRepositoriesLastWeek: (organization: string) => Promise<void>;
-  sendGithubTopRepositoriesPerTeamLastThreeDays: (organization: string) => Promise<void>;
 };
