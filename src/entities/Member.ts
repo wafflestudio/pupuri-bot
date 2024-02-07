9e511134f9494541c93636a67bbd1f636f00982e
woohm402 <woohm404@gmail.com>
2024-02-07 23:45:31 +0900

    멤버에 peng-u-0807 추가

--- src/entities/Member.ts
+++ src/entities/Member.ts
@@ -1,4 +1,4 @@
-export enum Member {
-  WOOHM402 = 'WOOHM402',
-  JUTAK97 = 'JUTAK97',
-}
+// 와플 정책상 유저의 식별자로는 github username 을 이용한다.
+export const members = ['woohm402', 'JuTaK97', 'peng-u-0807'] as const;
+
+export type Member = (typeof members)[number];
