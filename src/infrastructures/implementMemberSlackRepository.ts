ce5515ae6d3b380ca126b4c8042c682518507d86
woohm402 <woohm404@gmail.com>
2024-07-03 00:33:08 +0900

    node 버전업

--- src/infrastructures/implementMemberSlackRepository.ts
+++ src/infrastructures/implementMemberSlackRepository.ts
@@ -1,3 +1,5 @@
+import pLimit from 'p-limit';
+
 import { Member } from '../entities/Member';
 
 export const implementMemberSlackRepository = ({
@@ -14,30 +16,30 @@ export const implementMemberSlackRepository = ({
 
       if (!users.ok) throw new Error('Failed to fetch users');
 
+      const limit = pLimit(10);
+
+      const fetchMemberProfile = async (member: { id: string }) => {
+        const profile = await fetch(`https://slack.com/api/users.profile.get?user=${member.id}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${slackAuthToken}` },
+        }).then(
+          (res) =>
+            res.json() as Promise<
+              { ok: true; profile: { fields: { [githubField]?: { value: string } } } } | { ok: false }
+            >,
+        );
+
+        if (!profile.ok) throw new Error('Failed to fetch profile: ' + JSON.stringify(profile));
+
+        const githubUsername = profile.profile.fields[githubField]?.value;
+
+        if (!githubUsername) return [];
+
+        return [{ githubUsername: githubUsername.replace('https://github.com/', ''), slackUserId: member.id }];
+      };
+
       return {
-        members: (
-          await Promise.all(
-            users.members.flatMap(async (member) => {
-              const profile = await fetch(`https://slack.com/api/users.profile.get?user=${member.id}`, {
-                method: 'GET',
-                headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${slackAuthToken}` },
-              }).then(
-                (res) =>
-                  res.json() as Promise<
-                    { ok: true; profile: { fields: { [githubField]?: { value: string } } } } | { ok: false }
-                  >,
-              );
-
-              if (!profile.ok) throw new Error('Failed to fetch profile: ' + JSON.stringify(profile));
-
-              const githubUsername = profile.profile.fields[githubField]?.value;
-
-              if (!githubUsername) return [];
-
-              return [{ githubUsername: githubUsername.replace('https://github.com/', ''), slackUserId: member.id }];
-            }),
-          )
-        ).flat(),
+        members: (await Promise.all(users.members.map((member) => limit(() => fetchMemberProfile(member))))).flat(),
       };
     },
   };
