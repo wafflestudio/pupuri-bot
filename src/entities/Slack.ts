9d7974d29497f77ed2834ca705eda674668b6627
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2025-03-17 15:14:48 +0900

    ⬆️ bump (#98)

--- src/entities/Slack.ts
+++ src/entities/Slack.ts
@@ -2,8 +2,6 @@ export enum Emoji {
   first_place_medal = ':first_place_medal:',
   second_place_medal = ':second_place_medal:',
   third_place_medal = ':third_place_medal:',
-  four = ':four:',
-  five = ':five:',
   tada = ':tada:',
   'blob-clap' = ':blob-clap:',
   blobgamer = ':blobgamer:',
