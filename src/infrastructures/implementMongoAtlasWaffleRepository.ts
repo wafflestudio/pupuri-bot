b9383b63164fbd0161b955e4b3b319a31b198c61
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2025-03-28 18:16:00 +0900

    ✨ slash command 로 대시보드 볼 수 있도록 추가 (#99)

--- src/infrastructures/implementMongoAtlasWaffleRepository.ts
+++ src/infrastructures/implementMongoAtlasWaffleRepository.ts
@@ -3,11 +3,13 @@ import { z } from 'zod';
 import type { SlackID } from '../entities/Slack';
 import type { implementDashboardService } from '../services/DashboardService';
 import type { implementSlackEventService } from '../services/SlackEventService';
+import type { implementWaffleService } from '../services/WaffleService';
 
 export const implementMongoAtlasWaffleRepository = ({
   mongoDBUri,
 }: { mongoDBUri: string }): Parameters<typeof implementSlackEventService>[0]['waffleRepository'] &
-  Parameters<typeof implementDashboardService>[0]['waffleRepository'] => {
+  Parameters<typeof implementDashboardService>[0]['waffleRepository'] &
+  Parameters<typeof implementWaffleService>[0]['waffleRepository'] => {
   return {
     insert: async (records) => {
       const client = new MongoClient(mongoDBUri, { serverApi: ServerApiVersion.v1 });
@@ -23,6 +25,24 @@ export const implementMongoAtlasWaffleRepository = ({
         .collection('logs')
         .find({ date: { $gte: from, $lt: to } })
         .toArray();
+      await client.close();
+      const logSchema = z.object({
+        from: z.string(),
+        to: z.string(),
+        count: z.number(),
+        href: z.string(),
+        date: z.date(),
+      });
+      return {
+        logs: logs
+          .map((l) => logSchema.parse(l))
+          .map((log) => ({ ...log, from: log.from as SlackID, to: log.to as SlackID })),
+      };
+    },
+    listAllLogs: async () => {
+      const client = new MongoClient(mongoDBUri, { serverApi: ServerApiVersion.v1 });
+      await client.connect();
+      const logs = await client.db('waffle').collection('logs').find().toArray();
       await client.close();
       const logSchema = z.object({
         from: z.string(),
