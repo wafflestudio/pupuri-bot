1b099d19360742a3ac777d66030783aed5d671b1
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2025-04-04 23:27:01 +0900

    ✨ migrate to truffle (#113)

--- src/main.test.ts
+++ src/main.test.ts
@@ -38,6 +38,7 @@ const mockMongoDbCollection = mock(() => {
 const mockMongoDb = mock(() => {
   return { collection: mockMongoDbCollection };
 });
+const mockTruffleCapture = mock(() => Promise.resolve());
 const mockWadotListUsers = mock(() => {
   return Promise.resolve([
     { slack_id: 'QWER', github_id: 'qwer' },
@@ -52,6 +53,7 @@ const deps = {
   slackClient: { postMessage: mockSlackPostMessage, getPermalink: mockSlackGetPermalink },
   mongoClient: { db: mockMongoDb as unknown as Deps['mongoClient']['db'] },
   wadotClient: { listUsers: mockWadotListUsers },
+  truffleClient: { capture: mockTruffleCapture },
 } as unknown as Deps;
 
 const clearMocks = () => {
@@ -410,10 +412,11 @@ describe('status 500 on error', () => {
   test('should work', async () => {
     const response = await handle(deps, env, null as unknown as Request);
     expect(response.status).toBe(500);
-    expect(deps.slackClient.postMessage).toBeCalledTimes(1);
-    expect(deps.slackClient.postMessage).toBeCalledWith({
-      channel: 'C05021XHMQV',
-      text: `null is not an object (evaluating 'request.url')`,
-    });
+    expect(deps.truffleClient.capture).toBeCalledTimes(1);
+
+    const param = mockTruffleCapture.mock.calls.at(0)?.at(0) as unknown as Error;
+
+    expect(param).toBeInstanceOf(Error);
+    expect(param?.message).toBe("null is not an object (evaluating 'request.url')");
   });
 });
